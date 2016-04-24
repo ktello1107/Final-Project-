eeae37a29073a5896cd39603d4064d643a3ff150
ktello1107 <ktello326@robertmorris.edu>
2016-04-24 12:32:53 -0500

    Update Student_Informaton table

---
+++ Projects/App_Data/dbo.Student_Information.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Student_Information] (
+    [RollNo]     INT           NOT NULL,
+    [Name]       TEXT NULL,
+    [Department] TEXT NULL,
+    [DOB]        DATE NULL,
+    [Address]    TEXT NULL,
+    [MNo]        NTEXT           NULL,
+    [EID]        TEXT NULL,
+    [Notes]      TEXT NULL,
+    PRIMARY KEY CLUSTERED ([RollNo] ASC)
+);
+
